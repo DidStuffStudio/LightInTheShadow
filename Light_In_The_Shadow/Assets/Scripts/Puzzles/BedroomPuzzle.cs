82cac13312cddd853beb01567c3700f5db58e6a0
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2021-04-13 16:31:56 +0200

    Started work on scene 2 bedroom puzzle

---
+++ Light_In_The_Shadow/Assets/Scripts/Puzzles/BedroomPuzzle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Puzzles;
+using UnityEngine;
+
+public class BedroomPuzzle : PuzzleMaster
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        
+        
+        base.Update();
+    }
+}
